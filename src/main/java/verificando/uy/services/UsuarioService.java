8d52da3be45f3e7d3798b82e1abd0bc7246bdc12
juanpabloparentini <juanpabloparentini@gmail.com>
2024-11-10 23:10:48 +0000

    tests en postman y ajustes en ci

--- src/main/java/verificando/uy/services/UsuarioService.java
+++ src/main/java/verificando/uy/services/UsuarioService.java
@@ -21,35 +21,36 @@ public class UsuarioService {
     }
 
     public Usuario obtenerUsuarioPorId(Long id) {
-        // Lógica para obtener un usuario de la base de datos
         return usuarioRepository.findById(id).orElse(null);
     }
 
     public Usuario obtenerUsuarioPorEmail(String email) {
-        // Usar el método personalizado findByEmail
         return usuarioRepository.findByEmail(email);
     }
 
     public Usuario obtenerUsuarioPorCedula(String cedula) {
-        // Usar el método personalizado findByCedula
-        return usuarioRepository.findByCedula(cedula);
+        // Agregar prefijo antes de buscar en la base de datos
+        String cedulaFormato = "uy-ci-" + cedula;
+        return usuarioRepository.findByCedula(cedulaFormato);
     }
 
     public Usuario actualizarUsuario_Cedula(String cedula, Usuario usuarioActualizado) {
-        Usuario usuarioPorCedula = usuarioRepository.findByCedula(cedula);
+        String cedulaFormato = "uy-ci-" + cedula;
+        Usuario usuarioPorCedula = usuarioRepository.findByCedula(cedulaFormato);
+        
         if (usuarioPorCedula != null) {
             Long id = usuarioPorCedula.getid();
-    
+
             Optional<Usuario> usuarioExistente = usuarioRepository.findById(id);
             if (usuarioExistente.isPresent()) {
                 Usuario usuario = usuarioExistente.get();
-    
+
                 usuario.setFullName(usuarioActualizado.getFullName());
                 usuario.setEmail(usuarioActualizado.getEmail());
                 usuario.setId_token(usuarioActualizado.getId_token());
                 usuario.setRefresh_token(usuarioActualizado.getRefresh_token());
                 usuario.setPassword(usuarioActualizado.getPassword()); // Solo si es necesario actualizar la contraseña
-    
+
                 return usuarioRepository.save(usuario);
             } else {
                 throw new RuntimeException("Usuario no encontrado por ID.");
@@ -58,16 +59,12 @@ public class UsuarioService {
             throw new RuntimeException("Usuario no encontrado por cédula.");
         }
     }
-    
-    
 
     public void eliminarUsuario(Long id) {
-        // Lógica para eliminar un usuario de la base de datos
         usuarioRepository.deleteById(id);
     }
 
     public Usuario crearUsuario(Usuario usuario) {
-        // Lógica para crear un nuevo usuario en la base de datos
         return usuarioRepository.save(usuario);
     }
 }
